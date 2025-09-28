0a901e458957c6a8f93659d1c261ba3b0b6eb101
sheraz61 <hsheraz271@gmail.com>
2025-09-28 23:54:06 +0500

    port issue adjustment

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ dotenv.config({ path: '.env' });
 const startServer = async () => {
   try {
     await connectDB();
-    const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 5000;
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
