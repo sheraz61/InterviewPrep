e67fe282c08d1483f801ca597c7f1a99d612d676
sheraz61 <hsheraz271@gmail.com>
2025-09-29 10:23:58 +0500

    cors error remove

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173','https://interview-p.netlify.app/'],
+  origin: 'https://interview-p.netlify.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Authorization', 'X-Refresh-Token', 'Content-Type'],
   credentials: true
